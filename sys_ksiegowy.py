13a3d9948d1dda037ed9d879841a49f89ecda08b
SandraS <sandra.siedlecka@opoczta.pl>
2026-06-02 21:40:47 +0200

    Uruchomienie programu

---
+++ sys_ksiegowy.py
@@ -0,0 +1,12 @@
+operacje = ["saldo", "sprzedaż", "zakup", "konto", "lista", "magazyn", "przeglad", "koniec"]
+operacja = input(f"Podaj operację jaką chcesz wykonać: {operacje[::]}\n")
+# match operacja:
+#     case "saldo":
+#     case "sprzedaż":
+#     case "zakup":
+#     case "konto":
+#     case "lista":
+#     case "magazyn":
+#     case "zprzeglad":
+#     case "koniec":
+
