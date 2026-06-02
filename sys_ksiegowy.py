33a85c3ed5b5e595cadacb2f82123d025def1478
SandraS <sandra.siedlecka@opoczta.pl>
2026-06-02 21:54:09 +0200

    Operacja saldo

--- sys_ksiegowy.py
+++ sys_ksiegowy.py
@@ -1,7 +1,12 @@
 operacje = ["saldo", "sprzedaż", "zakup", "konto", "lista", "magazyn", "przeglad", "koniec"]
+stan_konta = 0
 operacja = input(f"Podaj operację jaką chcesz wykonać: {operacje[::]}\n")
-# match operacja:
-#     case "saldo":
+match operacja:
+    case "saldo":
+        a = int(input("Podaj kwotę, którą mam dodać do stanu konta (użyj - jeśli chcesz tę kwotę odjąć): "))
+        #if isinstance(a, int|float):
+        stan_konta+=a
+        print (f"Aktualny stan konta wynosi: {stan_konta} zł")
 #     case "sprzedaż":
 #     case "zakup":
 #     case "konto":
